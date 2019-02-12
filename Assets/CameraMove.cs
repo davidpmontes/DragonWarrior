5cf483bc3c2df9b7f9bf70dbc62546dcc5fef3d9
David Montes <david.montes@sjsu.edu>
2019-02-11 16:14:48 -0800

    added moving camera

---
+++ Assets/CameraMove.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public GameObject warrior;
+
+    void Update()
+    {
+        transform.position = new Vector3((float)(System.Math.Round(warrior.transform.position.x, 2)),
+                                          (float)(System.Math.Round(warrior.transform.position.y, 2)),
+                                         -10);
+    }
+}
