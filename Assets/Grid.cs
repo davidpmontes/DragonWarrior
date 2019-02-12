652b78e8ebb2afa35901f924e51ec5dfb8adccc8
David Montes <david.montes@sjsu.edu>
2019-02-11 23:50:07 -0800

    teleportation kind of works but relocating warrior doesnt

---
+++ Assets/Grid.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Grid : MonoBehaviour
+{
+    public struct MapAndCoordinate
+    {
+        public string tilemap;
+        public Vector2 coordinate;
+
+        public MapAndCoordinate(string tilemap, Vector2 coordinate)
+        {
+            this.tilemap = tilemap;
+            this.coordinate = coordinate;
+        }
+    }
+
+    public static Grid Instance;
+
+    public Tilemap Overworld;
+    public Tilemap OverworldInvisible;
+
+    public Tilemap Brecconary;
+    public Tilemap BrecconaryInvisible;
+
+    public Tilemap currentMap;
+    public Tilemap invisibleMap;
+
+    private List<string> MoveableTiles;
+    private Dictionary<string, Tilemap> StringToTilemap;
+    private Dictionary<MapAndCoordinate, MapAndCoordinate> TeleportLookup;
+    private Dictionary<string, float> MoveDelay;
+
+    void Start()
+    {
+        currentMap = Brecconary;
+        invisibleMap = BrecconaryInvisible;
+
+        Instance = this;
+
+        MoveableTiles = new List<string>();
+        MoveableTiles.Add("barrier");
+        MoveableTiles.Add("bricks");
+        MoveableTiles.Add("bridge");
+        MoveableTiles.Add("cave");
+        MoveableTiles.Add("castle");
+        MoveableTiles.Add("chest");
+        MoveableTiles.Add("desert");
+        MoveableTiles.Add("downStart");
+        MoveableTiles.Add("hills");
+        MoveableTiles.Add("plain");
+        MoveableTiles.Add("swamp");
+        MoveableTiles.Add("town");
+        MoveableTiles.Add("trees");
+        MoveableTiles.Add("upStair");
+        MoveableTiles.Add("stairs_outdoors");
+
+        MoveDelay = new Dictionary<string, float>();
+        MoveDelay.Add("hills", 0.1f);
+
+        PopulateTeleportLookup();
+
+        StringToTilemap = new Dictionary<string, Tilemap>();
+        StringToTilemap.Add("Overworld", Overworld);
+        StringToTilemap.Add("Brecconary", Brecconary);
+    }
+
+    void PopulateTeleportLookup()
+    {
+        TeleportLookup = new Dictionary<MapAndCoordinate, MapAndCoordinate>();
+
+        TeleportLookup.Add(new MapAndCoordinate("BreconnaryInvisible", new Vector2(-9, -7)), new MapAndCoordinate("Overworld", new Vector2(4, 1)));
+    }
+
+    public float GetMoveDelay(Vector3 pos)
+    {
+        if (MoveDelay.ContainsKey(GetTile(pos)))
+        {
+            return MoveDelay[GetTile(pos)];
+        }
+        return 0;
+    }
+
+    public string GetTile(Vector3 pos)
+    {
+        return currentMap.GetTile(new Vector3Int(Mathf.FloorToInt(pos.x),
+                                                 Mathf.FloorToInt(pos.y), 0)).name;
+    }
+
+    public bool TileIsMoveable(Vector3 pos)
+    {
+        return MoveableTiles.Contains(GetTile(pos));
+    }
+
+
+    public Vector2 CheckTeleport(Vector2 pos)
+    {
+        TileBase t = invisibleMap.GetTile(new Vector3Int(Mathf.FloorToInt(pos.x),
+                                                Mathf.FloorToInt(pos.y), 0));
+
+        if (t != null && t.name == "teleport")
+        {
+            MapAndCoordinate destination = TeleportLookup[new MapAndCoordinate(invisibleMap.name, pos)];
+
+            currentMap.gameObject.SetActive(false);
+            currentMap = StringToTilemap[destination.tilemap];
+            invisibleMap = StringToTilemap[destination.tilemap].transform.GetChild(0).gameObject.GetComponent<Tilemap>();
+            currentMap.gameObject.SetActive(true);
+            return destination.coordinate;
+        }
+        return pos;
+    }
+}
--- Assets/Scripts/Warrior.cs
+++ Assets/Scripts/Warrior.cs
@@ -4,8 +4,6 @@ using UnityEngine.Tilemaps;
 
 public class Warrior : MonoBehaviour
 {
-    public Tilemap tilemap;
-
     private Animator animator;
 
     private int horizontal_input;
@@ -13,13 +11,10 @@ public class Warrior : MonoBehaviour
 
     private bool isMoving;
     private bool isMovingDelayed;
-    private Vector2 moveLocation;
+    private Vector3 moveLocation;
 
     private readonly float MOVE_SPEED = 3f;
     private readonly float HOLD_KEY_LENGTH = 0.1f;
-
-    private List<string> MoveableTiles;
-    private Dictionary<string, float> MoveDelay;
 
     private string LeftTile;
     private string RightTile;
@@ -32,17 +27,6 @@ public class Warrior : MonoBehaviour
     private void Start()
     {
         animator = GetComponent<Animator>();
-
-        MoveableTiles = new List<string>();
-        MoveableTiles.Add("plain");
-        MoveableTiles.Add("trees");
-        MoveableTiles.Add("hills");
-        MoveableTiles.Add("cave");
-        MoveableTiles.Add("castle");
-        MoveableTiles.Add("town");
-
-        MoveDelay = new Dictionary<string, float>();
-        MoveDelay.Add("hills", 0.1f);
 
         moveLocation = Vector2.zero;
     }
@@ -77,61 +61,40 @@ public class Warrior : MonoBehaviour
 
         if (!isMoving)
         {
-            LeftTile = tilemap.GetTile(new Vector3Int(Mathf.FloorToInt(transform.position.x) - 1,
-                                                      Mathf.FloorToInt(transform.position.y), 0)).name;
-            RightTile = tilemap.GetTile(new Vector3Int(Mathf.FloorToInt(transform.position.x) + 1,
-                                                       Mathf.FloorToInt(transform.position.y), 0)).name;
-            UpTile = tilemap.GetTile(new Vector3Int(Mathf.FloorToInt(transform.position.x),
-                                                    Mathf.FloorToInt(transform.position.y) + 1, 0)).name;
-            DownTile = tilemap.GetTile(new Vector3Int(Mathf.FloorToInt(transform.position.x),
-                                                      Mathf.FloorToInt(transform.position.y) - 1, 0)).name;
+            Vector3 newDirection = Vector3.zero;
 
             if (horizontal_input == -1)
             {
                 animator.Play("WalkLeft");
-
-                if (!KeyHeldLongEnough())
-                    return;
-
-                if (MoveableTiles.Contains(LeftTile))
-                {
-                    moveLocation += new Vector2(-1, 0);
-                }
+                newDirection = new Vector3(-1, 0, 0);
             }
             else if (horizontal_input == 1)
             {
                 animator.Play("WalkRight");
-
-                if (!KeyHeldLongEnough())
-                    return;
-
-                if (MoveableTiles.Contains(RightTile))
-                    moveLocation += new Vector2(1, 0);
+                newDirection = new Vector3(1, 0, 0);
             }
             else if (vertical_input == 1)
             {
                 animator.Play("WalkUp");
-
-                if (!KeyHeldLongEnough())
-                    return;
-
-                if (MoveableTiles.Contains(UpTile))
-                    moveLocation += new Vector2(0, 1);
+                newDirection = new Vector3(0, 1, 0);
             }
             else if (vertical_input == -1)
             {
                 animator.Play("WalkDown");
-
-                if (!KeyHeldLongEnough())
-                    return;
-
-                if (MoveableTiles.Contains(DownTile))
-                    moveLocation += new Vector2(0, -1);
+                newDirection = new Vector3(0, -1, 0);
             }
             else
             {
                 isNoInput = true;
                 return;
+            }
+
+            if (!KeyHeldLongEnough())
+                return;
+
+            if (Grid.Instance.TileIsMoveable(transform.position + newDirection))
+            {
+                moveLocation += newDirection;
             }
 
             isMoving = true;
@@ -144,16 +107,13 @@ public class Warrior : MonoBehaviour
             transform.position = moveLocation;
             isMoving = false;
 
-            if (MoveDelay.ContainsKey((tilemap.GetTile(new Vector3Int(Mathf.FloorToInt(transform.position.x),
-                                                          Mathf.FloorToInt(transform.position.y), 0)).name)))
+            transform.position = Grid.Instance.CheckTeleport(transform.position);
+
+            float delayTime = Grid.Instance.GetMoveDelay(transform.position);
+            if (delayTime > 0)
             {
-                float delayTime = MoveDelay[(tilemap.GetTile(new Vector3Int(Mathf.FloorToInt(transform.position.x),
-                                              Mathf.FloorToInt(transform.position.y), 0)).name)];
-                if (delayTime > 0)
-                {
-                    isMovingDelayed = true;
-                    Invoke("RemoveDelay", delayTime);
-                }
+                isMovingDelayed = true;
+                Invoke("RemoveDelay", delayTime);
             }
         }
     }
